7bc1c4b3b6f1713ec3d84c422f40521de35f6463
c-sten <c.tendermann@voco.ee>
2025-01-31 13:30:41 +0200

    punkt2/punkt7
    base

---
+++ bubble bluster/game.py
@@ -0,0 +1,20 @@
+import pygame
+from settings import Settings
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
+    
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
+    running = True
+    while running:
+        screen.fill(gm_settings.bg_color)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+        pygame.display.flip()
+
+    pygame.quit()
+
+run_game()
