bc63d27b770b819b76c859600f86ab50afc47c7f
c-sten <c.tendermann@voco.ee>
2025-02-05 12:49:08 +0200

    punkt4
    punkt4

---
+++ bubblebluster/game_functions.py
@@ -0,0 +1,44 @@
+import sys
+import pygame
+from bubble import Bubble
+
+pygame.init()
+ADDBUBBLE = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDBUBBLE, 250)
+
+def check_events(game_settings, screen, player, bubbles):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                player.moving_right = True
+            if event.key == pygame.K_LEFT:
+                player.moving_left = True
+            if event.key == pygame.K_UP:
+                player.moving_up = True
+            if event.key == pygame.K_DOWN:
+                player.moving_down = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                player.moving_right = False
+            if event.key == pygame.K_LEFT:
+                player.moving_left = False
+            if event.key == pygame.K_UP:
+                player.moving_up = False
+            if event.key == pygame.K_DOWN:
+                player.moving_down = False
+        elif event.type == ADDBUBBLE:
+            create_bubble(game_settings, screen, bubbles)
+
+def create_bubble(game_settings, screen, bubbles):
+    new_bubble = Bubble(screen, game_settings)
+    bubbles.add(new_bubble)
+        
+            
+def update_screen(game_settings, screen, player, bubbles):
+    screen.fill(game_settings.bg_color)
+    player.blit_me()
+    for bubble in bubbles:
+        bubble.blit_me()
+    pygame.display.flip()
---
+++ bubblebluster/mäng.py
@@ -0,0 +1,23 @@
+import pygame
+from settings import Settings
+from player import Player
+from bubble import Bubble
+import game_functions as gf
+
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
+    
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
+    
+    player = Player(screen)
+    bubbles = pygame.sprite.Group()
+    
+    while True:
+        gf.check_events(gm_settings, screen, player, bubbles)
+        player.update()
+        bubbles.update()
+        gf.update_screen(gm_settings, screen, player, bubbles)
+    
+run_game()
