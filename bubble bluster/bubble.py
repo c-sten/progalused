aeedd46111b5cc4257889b96aa6aa99a0e0a21ea
c-sten <c.tendermann@voco.ee>
2025-02-05 12:46:13 +0200

    punkt4/punkt7
    punkt4/punkt7, mullikesi tundub kuidagi vähevõitu olevat?

--- bubble bluster/bubble.py
+++ bubble bluster/bubble.py
@@ -9,7 +9,7 @@ class Bubble(pygame.sprite.Sprite):
         self.screen_rect = screen.get_rect()
         
         self.bubble_radius = random.randint(game_settings.bubble_min_r, game_settings.bubble_max_r)
-        self.bubble = pygame.Surface((self.bubble_radius * 2, self.bubble_radius * 2), pygame.SCRALPHA)
+        self.bubble = pygame.Surface((self.bubble_radius * 2, self.bubble_radius * 2), pygame.SRCALPHA)
         self.bubble.set_colorkey(game_settings.bg_color)
         self.bubble.set_alpha(128)
         self.rect = pygame.draw.circle(
